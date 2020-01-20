4025fa32c30d710fe37e757bbff4f08a31b45b43
febusch <busch@soziologie.uzh.ch>
2020-01-20 15:45:55 +0100

    added 2nd training

---
+++ build_scraper_1.py
@@ -0,0 +1,14 @@
+def main():
+    """
+    Use this function to build are scraper for eth-gethired.
+    The objective is to print the number of ads in the past 7 days.
+    To achieve this, with the 'requests' module.
+
+    In this example, we have no problems parsing the data.
+    However, accessing the information in a good way requires understanding the network traffic.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ build_scraper_2.py
@@ -0,0 +1,13 @@
+def main():
+    """
+    Use this function to build are scraper for suedostschweizjobs.ch.
+    The objective is to print the number of ads in the past 7 days.
+    To achieve this, with the 'requests' and the 'bs4' modules.
+
+    In this example, we have to iterate over pages. Parsing of HTML code has to be done.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
