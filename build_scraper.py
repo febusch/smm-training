a48d32b562624421d8e3debbb13d9c59a310cd41
febusch <busch@soziologie.uzh.ch>
2020-01-20 10:43:21 +0100

    added main

---
+++ build_scraper.py
@@ -0,0 +1,11 @@
+def main():
+    """
+    This function prints the number of ads currently posted on eth-gethired.
+
+    To achieve this, with the 'requests' module.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
