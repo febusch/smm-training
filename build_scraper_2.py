854d4407f497fb78e998205f9b64c356497ff380
Austin George <austin.george@uzh.ch>
2020-01-22 13:19:39 +0100

    scraper 2

--- build_scraper_2.py
+++ build_scraper_2.py
@@ -1,3 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+from math import floor
+from datetime import datetime, timedelta
+
+
 def main():
     """
     Use this function to build are scraper for suedostschweizjobs.ch.
@@ -6,8 +12,64 @@ def main():
 
     In this example, we have to iterate over pages. Parsing of HTML code has to be done.
     """
-    pass
+
+    max_page_count = get_max_page_count()
+
+    jobs = []
+
+    for i in range(max_page_count):
+
+        print("{}%\tFound {} job(s)".format(int(i/max_page_count*100), len(jobs)))
+        current_page = page_source(i)
+        articles = current_page.find_all('article')
+
+        for article in articles:
+            article = article.find('div', {'class': 'job__content clearfix'})
+
+            date = article.find('span', {'class': 'date'}).text
+            date = date.strip().replace(",", "")
+            date = datetime.strptime(date, '%d.%m.%Y').date()
+
+            a_week_ago = datetime.now().date() - timedelta(days=7)
+
+            if date >= a_week_ago:
+
+                job = {
+
+                    "startDate": str(date),
+                    "title": article.find('a', {'class': 'recruiter-job-link'}).text,
+                    "location": article.find('div',  {'class': 'location'}).text.replace("\n", ""),
+                    "recruiter": article.find('span',  {'class': 'recruiter-company-profile-job-organization'}).text.replace("\n", ""),
+                    "url": article.find('a').get('href')
+
+                }
+
+                jobs.append(job)
+
+    return jobs
+
+
+def page_source(n):
+
+    page = requests.get('https://www.suedostschweizjobs.ch/jobs?page=' + str(n)).text
+    source = BeautifulSoup(page, "lxml")
+
+    return source
+
+
+def get_max_page_count():
+
+    soup = page_source(0)
+
+    job_count_string = soup.find('h1', {'class': 'search-result-header'}).text
+    job_count_int = int(job_count_string.replace(" Jobs", "").replace("'", ""))
+    job_count = floor(job_count_int / 20)
+
+    return job_count
 
 
 if __name__ == '__main__':
-    main()
+
+    allJobs = main()
+    print(len(allJobs))
+    print(allJobs)
