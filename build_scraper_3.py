bdc0d4c504f5e0f874ae197e310edbb7375344ce
febusch <busch@soziologie.uzh.ch>
2020-01-23 10:33:44 +0100

    added scraper training 3

---
+++ build_scraper_3.py
@@ -0,0 +1,14 @@
+def main():
+    """
+    Use this function to build a scraper for content of jobroom.ch.
+    The objective is to extract the content of an ad, in this case located
+    at https://www.job-room.ch/job-search/dea5789c-3dc1-11ea-ab6f-005056ac086d
+
+    Since the content of this page is rendered dynamically, we have to use
+    selenium webdriver to extract it.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
